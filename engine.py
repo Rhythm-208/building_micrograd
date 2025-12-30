a1ef679e44ac8b72385c0ba63b823b89fd542cf9
Rhythm <rhythmjunejasonu@gmail.com>
2025-12-30 21:42:10 +0530

    Improvements

--- engine.py
+++ engine.py
@@ -12,28 +12,56 @@ class Value:
         return f'Value(data= {self.data})'
 
     def __add__(self,other):
+        other = other if isinstance(other,Value) else Value(other)
         out = Value(self.data + other.data,(self,other),'+')
         def _backward():
-            self.grad=1.0*out.grad
-            other.grad= 1.0*out.grad
+            self.grad+=1.0*out.grad
+            other.grad+= 1.0*out.grad
 
         out._backward=_backward
         return out
 
     def __mul__(self,other):
+        other =other if isinstance(other,Value) else Value(other)
         out = Value(self.data * other.data,(self,other),'*')
         def _backward():
-            self.grad=(other.data*out.grad)
-            other.grad=(self.data)*out.grad
+            self.grad+=(other.data*out.grad)
+            other.grad+=(self.data)*out.grad
 
         out._backward= _backward
         return out
+
+    def __pow__(self,other):
+        assert isinstance(other,(int,float))
+        out = Value(self.data**other,(self,),f'**{other}')
+        def _backward():
+            self.grad+=other*(self.data**(other-1))*out.grad
+        out._backward=_backward
+        return out
+    def __truediv__(self,other):
+        return self * (other**-1)
+    def __rmul__(self,other):
+        return self*other
+
+    def exp(self):
+        x=self.data
+        out = Value(math.exp(x),(self,),'exp')
+        def _backward():
+            self.grad+=out.grad*out.data
+        out._backward=_backward
+        return out
+    def __neg__(self):
+        return self*-1
+    def __sub__(self,other):
+        return self +(-other)
+
+
     def tanh(self):
         n=self.data
         t=(math.exp(2*n)-1)/(math.exp(2*n)+1)
         out=Value(t,(self,),'tanh')
         def _backward():
-            self.grad=out.grad*(1-t**2)
+            self.grad+=out.grad*(1-t**2)
 
         out._backward= _backward
         return out
